fdb52fd89c1837e2c4e8d6663aac41753a82616d
unknown <baharcandir75@gmail.com>
2019-02-01 20:28:40 -0600

    branches learning

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -13,30 +13,43 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.testng.Assert.*;
-
 public class LoginTests {
+
     WebDriver driver;
+
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-
     }
+
+
 
     @Test
     public void loginTest1() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-        assertEquals(driver.getTitle(), "Web Orders");
-    }
-    @AfterMethod
-    public void cleanUp(){
-        driver.close();
+        Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
 
+    @Test
+    public void logOutTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+
+        driver.findElement(By.id("ctl00_logout")).click();
+
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+
+    }
+
+
+    @AfterMethod
+    public void cleanUp() {
+        driver.close();
+    }
 
 }
